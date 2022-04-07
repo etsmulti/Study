67a4a8c6a753b60979d717646b6f041ec7a5d747
etsmulti <etsmulti@gmail.com>
2022-04-07 15:31:26 +0900

    Add files via upload

--- yh_sum/src/yh_sum_pub.cpp
+++ yh_sum/src/yh_sum_pub.cpp
@@ -1,30 +1,58 @@
 #include "ros/ros.h"
 #include "yh_sum/yh_sum_msg.h"
 
+class MySubClass
+{
+    public:
+        MySubClass()
+        {
+            pub = nh.advertise<yh_sum::yh_sum_msg>("yh_sum_topic", 100);
+            ros::Rate loop_rate(5);
+            yh_sum::yh_sum_msg msg;
+            int cnt = 0;
+
+            while(ros::ok())
+            {
+                msg.stamp = ros::Time::now();
+                msg.data = cnt;
+                ROS_INFO("send msg = %d", msg.stamp.sec);
+                ROS_INFO("send msg = %d", msg.stamp.nsec);
+                ROS_INFO("send msg = %d", msg.data);
+                cnt++;
+                pub.publish(msg);
+                loop_rate.sleep();
+            }
+        }
+
+    private:
+        ros::NodeHandle nh;
+        ros::Publisher pub;
+
+};
+
+
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "yh_sum_pub");
-    ros::NodeHandle nh;
 
-    ros::Publisher pub = nh.advertise<yh_sum::yh_sum_msg>("yh_sum_topic", 100);
+    MySubClass mysub;
 
-    ros::Rate loop_rate(5);
-
-    yh_sum::yh_sum_msg msg;
-
-    int cnt = 0;
-
-    while(ros::ok())
-    {
-        msg.stamp = ros::Time::now();
-        msg.data = cnt;
-        ROS_INFO("send msg = %d", msg.stamp.sec);
-        ROS_INFO("send msg = %d", msg.stamp.nsec);
-        ROS_INFO("send msg = %d", msg.data);
-        cnt++;
-        pub.publish(msg);
-        loop_rate.sleep();
-    }
+    // ros::NodeHandle nh;
+    // ros::Publisher pub = nh.advertise<yh_sum::yh_sum_msg>("yh_sum_topic", 100);
+    // ros::Rate loop_rate(5);
+    // yh_sum::yh_sum_msg msg;
+    // int cnt = 0;
+    // while(ros::ok())
+    // {
+    //     msg.stamp = ros::Time::now();
+    //     msg.data = cnt;
+    //     ROS_INFO("send msg = %d", msg.stamp.sec);
+    //     ROS_INFO("send msg = %d", msg.stamp.nsec);
+    //     ROS_INFO("send msg = %d", msg.data);
+    //     cnt++;
+    //     pub.publish(msg);
+    //     loop_rate.sleep();
+    // }
 
     return 0;
 
---
+++ yh_sum/src/yh_sum_pub_.cpp
@@ -0,0 +1,31 @@
+#include "ros/ros.h"
+#include "yh_sum/yh_sum_msg.h"
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "yh_sum_pub");
+    ros::NodeHandle nh;
+
+    ros::Publisher pub = nh.advertise<yh_sum::yh_sum_msg>("yh_sum_topic", 100);
+
+    ros::Rate loop_rate(5);
+
+    yh_sum::yh_sum_msg msg;
+
+    int cnt = 0;
+
+    while(ros::ok())
+    {
+        msg.stamp = ros::Time::now();
+        msg.data = cnt;
+        ROS_INFO("send msg = %d", msg.stamp.sec);
+        ROS_INFO("send msg = %d", msg.stamp.nsec);
+        ROS_INFO("send msg = %d", msg.data);
+        cnt++;
+        pub.publish(msg);
+        loop_rate.sleep();
+    }
+
+    return 0;
+
+}
