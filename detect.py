749dd42e6067002b259641ee3e2be58da8ed7a71
etsmulti <etsmulti@gmail.com>
2022-06-03 10:51:24 +0900

    Create detect.py
    얼굴 크롭된것 인식

---
+++ detect.py
@@ -0,0 +1,41 @@
+import tensorflow.keras
+from PIL import Image, ImageOps
+import numpy as np
+
+# Disable scientific notation for clarity
+np.set_printoptions(suppress=True)
+
+# Load the model
+model = tensorflow.keras.models.load_model('my_model_2.h5')
+
+# Create the array of the right shape to feed into the keras model
+# The 'length' or number of images you can put into the array is
+# determined by the first position in the shape tuple, in this case 1.
+data = np.ndarray(shape=(1, 96, 96, 3), dtype=np.float32)
+
+# Replace this with the path to your image
+image = Image.open('test_photo (2).jpg')
+
+#resize the image to a 224x224 with the same strategy as in TM2:
+#resizing the image to be at least 224x224 and then cropping from the center
+size = (96, 96)
+image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+#turn the image into a numpy array
+image_array = np.asarray(image)
+
+# display the resized image
+image.show()
+
+# Normalize the image
+normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+
+# Load the image into the array
+data[0] = normalized_image_array
+
+# run the inference
+prediction = model.predict(data)
+if prediction[0,0] > prediction[0,1]:
+    print(f'{prediction[0,0]}의 확률로 남성')
+else:
+    print(f'{prediction[0,1]}의 확률로 여성')
